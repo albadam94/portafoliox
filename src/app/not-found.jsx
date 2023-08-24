7bced640638db7dfadb8c1611b45395d55d8c1f6
Brayan Albadam <brayanalbadam@gmail.com>
2023-08-24 17:01:34 -0500

    update

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
+
 export default function NotFound() {
     return (
-        <main>
+    
         <div>
             <h1 className="ux2 flex justify-center text-4xl sm:text-5xl md:text-8xl font-bold max-w-7xl mx-auto px-4 sm:px-6 lg:px-9 mt-16">404</h1>
             <Image src="/Lumalee.svg" alt="Lumalee" width={500} height={200} className="mx-auto mb-5"/>
@@ -16,6 +17,6 @@ export default function NotFound() {
             </div>
          </Link>
         </div>
-        </main>
+        
     );
     }
