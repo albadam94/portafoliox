30d77b9386bfb42f5e5e502a581ef6e2b84b44ea
Brayan Albadam <brayanalbadam@gmail.com>
2023-09-22 16:33:52 -0500

    update

--- src/app/components/aexperiencia.jsx
+++ src/app/components/aexperiencia.jsx
@@ -33,18 +33,18 @@ export default function App() {
         <p className="text-sm">Realice la creación de la pagina de noticias ANC NOTICIAS con el fin de brindar información en noticias a nivel local, nacional e internacional, logrando un alcance hasta de 30.000 personas y alcanzar un 
         aproximado de 9500 seguidores en Facebook implementando al principio estrategias de marketing segmentadas en ciudades como Palmira, Cali, Medellín y Bogotá para posteriormente lograr un alcance orgánico .</p>
       </AccordionItem>
-      <AccordionItem className="text-xl font-semibold mb-2" key="2" aria-label="Web y Multimedia Developer | We Are Angular" title="Web y Multimedia Developer | We Are Angular">
-        <p className="text-sm">- Diseño y desarrollo de landings en Wordpress <br/>- Lider implementación Frontend de LMS en CANVAS para un proyecto de educación en agricultura para usuarios en 
-    paises de San Salvador, Honduras y Guatemala; Diseño Experiencia de usuario, Obtención de datos con API, Creación de modulos, Registros CSV, implementación HTML y CSS.
+      <AccordionItem className="text-xl font-semibold mb-2" key="2" aria-label="UX Engineer | We Are Angular" title="UX Engineer | We Are Angular">
+        <p className="text-sm">- Diseño y desarrollo de landings page en Wordpress <br/>- Lider implementación Frontend de LMS en CANVAS para un proyecto de educación en agricultura para usuarios en 
+    paises de San Salvador, Honduras y Guatemala; Diseño Experiencia de usuario UX, Obtención de datos con API y CSV, Creación de modulos, Registros CSV, implementación HTML y CSS.
     -Realice pruebas de usabilidad con el equipo de Learning Experience para conocer la interactividad de los usuarios con la aplicación.<br/>  
-    -Realice los diseños de Ux Flow, Ux Task, casos de uso del sistema PRM de aprendizaje y de cada uno de los cursos. <br/>
-    -Brinde apoyo en el diseño y prototipado realizando modificaciones en las interfaces de los cursos e implementarlos en la aplicación mediante codigo HTML y CSS tanto para agricultores como Extensionistas</p>
+    -Realice los diseños de UX Flow, UX Task, casos de uso del sistema PRM de aprendizaje y de cada uno de los cursos. <br/>
+    -Brinde apoyo en el diseño UI y prototipado realizando modificaciones en las interfaces de los cursos e implementandolos en la aplicación mediante codigo HTML y CSS tanto para agricultores como Extensionistas</p>
       </AccordionItem>
       </Accordion>
 
       <Accordion isCompact className="flex flex-col h-full p-4 border rounded-lg shadow-md text-lg font-medium mb-4">
       <AccordionItem className="text-xl font-semibold mb-10" key="1" aria-label="Gestor de Proyecto TI | Innpulsa-AppsCo-ICESI" title="Gestor de Proyecto TI | Innpulsa-AppsCo-ICESI">
-      <p className="text-sm">Líder de proyecto Mid Level, encargado de gestionar las actividades necesarias que debian realizar los diseñadores y desarrolladores, para ser entregados en los tiempos establecidos los productos mínimos viables que hicieron parte del Ciclo 2 del Proyecto Apps.Co- Fase Producto Digital.</p>
+      <p className="text-sm">Líder de proyecto Mid Level, encargado de gestionar las actividades necesarias que debian realizar los diseñadores y desarrolladores, para ser entregados en los tiempos establecidos los 74 productos mínimos viables que hicieron parte del Ciclo 2 del Proyecto Apps.Co- Fase Producto Digital.</p>
       </AccordionItem>
       </Accordion>
    
