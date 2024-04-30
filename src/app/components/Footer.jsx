f23e427474d81cbb1fd4adbb6546518a9e80b46f
Brayan Albadam <brayanalbadam@gmail.com>
2024-04-30 15:30:29 -0500

    nuevaversion

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center p-4">
         <div className="text-custom-blue text-sm font-normal leading-tight md:mx-4 lg:mx-12 xl:mx-20 font-['Archivo']">
           UX Albadam Diseño-Desarrollo en {''}
-          <a href="https://www.figma.com" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.figma.com/file/JkM0L29FITCtiedxzpqkxB/Portafolio-2024?type=design&node-id=643-60&mode=design" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon className='fa-2x w-[289px] h-6 justify-start items-start gap-[29px] inline-flex'
               icon={faFigma}
               style={{ color: '#083040', height: '24px', width: '24px' }}
