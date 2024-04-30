1bb9fbbc8680378c54eb923b0d55d9727b793613
Brayan Albadam <brayanalbadam@gmail.com>
2024-04-30 15:49:50 -0500

    nuevaversion

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        
+        <title>{metadata.title}</title>
         <meta name="title" content={metadata.title} />
         <meta name="description" content={metadata.description} />
         <meta name="author" content={metadata.author} />
