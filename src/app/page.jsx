38de820564e6d680bc9dbd04c25ebdf960feccf3
Brayan Albadam <brayanalbadam@gmail.com>
2024-05-02 15:20:54 -0500

    efectotexto

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
 
       <div className='mx-4 md:mx-12 lg:mx-20 xl:mx-80 md:mb-10'>
           <div className="text-center md:mt-28">
-          <h1 className="text-custom-blue Title text-4xl mb-3 mt-9 md:text-7xl font-bold  font-['Archivo'] md:mt-16 md:mb-6">Diseñador UX/UI<br></br>Frontend Developer</h1>
+          <h1 className="text-custom-blue Title text-4xl mb-3 mt-9 md:text-7xl font-bold  font-['Archivo'] md:mt-16 md:mb-6">Diseñador{""} <span className="UX">UX/UI</span><br></br>Frontend Developer</h1>
           <p className="text-Darkcharcoal text-base font-medium leading-15 leading-tight mb-2">Hola ✌️soy Brayan Albadam Diseñador multimedia y desarrollador frontend</p>
           <p className="text-Darkcharcoal text-base font-medium leading-15 leading-tight mb-4">Especializado en diseño de experiencia de usuario</p>
           <Icons />
