18abc6a149bb87586c9c984b2f461af77fcc8620
Brayan Albadam <brayanalbadam@gmail.com>
2025-08-24 17:35:04 -0500

    Cards

--- src/app/components/Proyectos.jsx
+++ src/app/components/Proyectos.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const proyectosData = [
   {
@@ -7,7 +8,7 @@ const proyectosData = [
     title: 'Proyecto ASAVirtual',
     description: 'Diseño e implementación de las interfaces y contenidos de aprendizaje para agricultores de centroamerica, diseño de experiencias, learning experience, implementación en HTML y CSS',
     imageUrl: '/CoverB.svg',
-    link: 'https://aulavirtualasa.com/',
+    link: 'https://www.behance.net/gallery/188355743/Proyecto-ASAVirtual',
   },
   {
     id: 2,
@@ -26,7 +27,7 @@ const proyectosData = [
   {
     id: 4,
     title: 'Diseño UI Tactiq Bizzell',
-    description: 'Diseño UI de una landing page que ofrece soluciones innovadoras en gestión de personal, logística, comunicaciones en salud, consultoría estratégica y tecnología avanzada, diseñadas específicamente para agencias federales. Diseñada en Figma',
+    description: 'Diseño UI de una landing page que ofrece soluciones innovadoras en gestión de personal, logística, comunicaciones en salud, consultoría estratégica y tecnología avanzada, diseñadas específicamente para agencias federales. diseñada en Figma',
     imageUrl: '/Ejercicios.svg',
     link: 'https://www.tactiq-bizzell.us/',
   },
@@ -39,9 +40,21 @@ const proyectosData = [
   },
 ];
 
-const Proyecto = ({ proyecto }) => {
+const Proyecto = ({ proyecto, index }) => {
   return (
-    <div className="max-w-[820px] md:h-auto mx-auto p-10 bg-white rounded-3xl shadow flex-col justify-start items-start gap-2.5 inline-flex font-['Geist']">
+   <motion.div
+  initial={{ y: 50 + index * 20, scale: 0.9, opacity: 0 }}
+  whileInView={{ y: 0, scale: 1, opacity: 1 }}
+  viewport={{ once: false, amount: 0.3 }}
+  transition={{ duration: 0.6, delay: index * 0.15 }}
+  className="max-w-[820px] mx-auto p-10 bg-white rounded-3xl flex-col justify-start items-start gap-2.5 inline-flex font-['Geist'] relative"
+  style={{
+    zIndex: proyectosData.length - index,
+    boxShadow:
+      '0 1px 2px 0 rgba(60, 64, 67, 0.30), 0 1px 3px 1px rgba(60, 64, 67, 0.15)',
+  }}
+>
+
       <div className="flex flex-col items-start gap-6 md:flex-row">
         
         {/* Contenedor de imagen con aspect-ratio */}
@@ -64,7 +77,7 @@ const Proyecto = ({ proyecto }) => {
           </p>
 
           <a
-            className="w-[175px] h-[47px] md:p-6 bg-Darkcharcoal rounded-xl border border-Darkcharcoal justify-center items-center gap-[50px] inline-flex mt-3 md:mt-4"
+            className="w-[175px] h-[47px] md:p-6 bg-Darkcharcoal rounded-xl border border-Darkcharcoal justify-center items-center inline-flex mt-3 md:mt-4"
             href={proyecto.link}
             rel="noopener noreferrer"
             target="_blank"
@@ -75,15 +88,15 @@ const Proyecto = ({ proyecto }) => {
           </a>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
 const ProyectosList = () => {
   return (
-    <div className="mt-14 flex flex-col gap-10">
-      {proyectosData.map((proyecto) => (
-        <Proyecto key={proyecto.id} proyecto={proyecto} />
+    <div className="mt-14 flex flex-col gap-10 relative">
+      {proyectosData.map((proyecto, index) => (
+        <Proyecto key={proyecto.id} proyecto={proyecto} index={index} />
       ))}
     </div>
   );
